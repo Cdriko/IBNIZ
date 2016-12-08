768562924e4336951f3c2d7c270b3ae862fe12df
Cedric Doutriaux <cdriko@free.fr>
2016-12-08 11:51:06 +0100

    force portrait layout

---
+++ ibnizoid/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/cedric/AndroidStudioProjects/f1fa_ibniz_clean/ibnizoid/bin/AndroidManifest.xml : \
+/home/cedric/AndroidStudioProjects/f1fa_ibniz_clean/ibnizoid/AndroidManifest.xml \
---
+++ ibnizoid/bin/SDLActivity.ap_.d
@@ -0,0 +1,12 @@
+/home/cedric/AndroidStudioProjects/f1fa_ibniz_clean/ibnizoid/bin/SDLActivity.ap_ \
+ : /home/cedric/AndroidStudioProjects/f1fa_ibniz_clean/ibnizoid/res/drawable-mdpi/ic_launcher.png \
+/home/cedric/AndroidStudioProjects/f1fa_ibniz_clean/ibnizoid/res/layout/main.xml \
+/home/cedric/AndroidStudioProjects/f1fa_ibniz_clean/ibnizoid/res/values/strings.xml \
+/home/cedric/AndroidStudioProjects/f1fa_ibniz_clean/ibnizoid/res/drawable-hdpi/ic_launcher.png \
+/home/cedric/AndroidStudioProjects/f1fa_ibniz_clean/ibnizoid/res/drawable-xxhdpi/ic_launcher.png \
+/home/cedric/AndroidStudioProjects/f1fa_ibniz_clean/ibnizoid/res/drawable-xhdpi/ic_launcher.png \
+/home/cedric/AndroidStudioProjects/f1fa_ibniz_clean/ibnizoid/bin/res/drawable-mdpi/ic_launcher.png \
+/home/cedric/AndroidStudioProjects/f1fa_ibniz_clean/ibnizoid/bin/res/drawable-hdpi/ic_launcher.png \
+/home/cedric/AndroidStudioProjects/f1fa_ibniz_clean/ibnizoid/bin/res/drawable-xxhdpi/ic_launcher.png \
+/home/cedric/AndroidStudioProjects/f1fa_ibniz_clean/ibnizoid/bin/res/drawable-xhdpi/ic_launcher.png \
+/home/cedric/AndroidStudioProjects/f1fa_ibniz_clean/ibnizoid/bin/AndroidManifest.xml \
