48155df36c8db38a2de8b25fbc1f5365e6bcde77
Cedric Doutriaux <cdriko@free.fr>
2016-12-08 11:53:00 +0100

    force portrait layout

--- ibnizoid/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/cedric/AndroidStudioProjects/f1fa_ibniz_clean/ibnizoid/bin/AndroidManifest.xml : \
-/home/cedric/AndroidStudioProjects/f1fa_ibniz_clean/ibnizoid/AndroidManifest.xml \
--- ibnizoid/bin/SDLActivity-debug-unaligned.apk.d
+++
@@ -1,9 +0,0 @@
-/home/cedric/AndroidStudioProjects/f1fa_ibniz_clean/ibnizoid/bin/SDLActivity-debug-unaligned.apk : \
-/home/cedric/AndroidStudioProjects/f1fa_ibniz_clean/ibnizoid/bin/SDLActivity.ap_ \
-/home/cedric/AndroidStudioProjects/f1fa_ibniz_clean/ibnizoid/bin/classes.dex \
-/home/cedric/AndroidStudioProjects/f1fa_ibniz_clean/ibnizoid/libs/x86/libmain.so \
-/home/cedric/AndroidStudioProjects/f1fa_ibniz_clean/ibnizoid/libs/x86/libSDL2.so \
-/home/cedric/AndroidStudioProjects/f1fa_ibniz_clean/ibnizoid/libs/armeabi-v7a/libmain.so \
-/home/cedric/AndroidStudioProjects/f1fa_ibniz_clean/ibnizoid/libs/armeabi-v7a/libSDL2.so \
-/home/cedric/AndroidStudioProjects/f1fa_ibniz_clean/ibnizoid/libs/armeabi/libmain.so \
-/home/cedric/AndroidStudioProjects/f1fa_ibniz_clean/ibnizoid/libs/armeabi/libSDL2.so \
